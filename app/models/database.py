64ec4c774925b604e49d6ded26b9e17b08793470
Xavier Celades <59837050+celades5@users.noreply.github.com>
2025-03-25 09:47:10 +0100

    Add files via upload

--- app/models/database.py
+++ app/models/database.py
@@ -2,21 +2,18 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# con = psycopg2.connect(
-#     dbname = "postgres",
-#     user = self.user_name,
-#     password = self.password, 
-#     host = '',
-# )
+# DATABASE_URL =  "db_type://db_user:password@host:port/db_name"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/TimeTracker"
 
-# con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-# cur = con.cursor()
-
-# cur.execute(sql.SQL())
-             # postgresql://username:password@host:port/database_name
-DATABASE_URL = "postgresql://TimeTracker:test@localhost:5432/timetracker"
-
+                                    # allowing multiple threads to share db connection
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/models/models.py
+++ app/models/models.py
@@ -1,14 +1,37 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from database import Base
+from .database import Base
+from datetime import datetime
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, index=True, unique=True)
-    task = relationship("Task", back_populates="owner")
+    tasks = relationship("Task", back_populates="owner")
     time_entries = relationship("TimeEntry", back_populates="user")
 
 class Task(Base):
-    __tablename__= 'tasks'   
+    __tablename__= "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(String(255), nullable=True)
+    isCompleted = Column(Boolean, default=False)
+    due_date = Column(DateTime)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    #One task belongs to one user
+    owner = relationship("User", back_populates="tasks")
+    #One task can have more than one time entry
+    time_entries = relationship("TimeEntry", back_populates="task")
+
+class TimeEntry(Base):
+    __tablename__= "time_entries"
+    id = Column(Integer, primary_key=True, index=True)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=True)
+
+    task_id = Column (Integer, ForeignKey('tasks.id'))
+    user_id = Column (Integer, ForeignKey('users.id'))
+
+    task = relationship("Task", back_populates="time_entries")
+    user = relationship("User", back_populates="time_entries")
